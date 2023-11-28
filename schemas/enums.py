5da5bb7eb9a1bbfbae5089b555cab036e3cdbc58
Александр Скрынник <cristiano051985@gmail.com>
2023-11-28 16:04:11 +0700

    Добавлены параметры для Яндекс диска

--- schemas/enums.py
+++ schemas/enums.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 
+DISK_URL = 'https://cloud-api.yandex.net/v1/disk/resources/'
+
 class Option(str, Enum):
     authorization = 'Authorization'
     resource = 'Resource'
@@ -11,49 +13,15 @@ class Resource(str, Enum):
 
 
 class Operation(str, Enum):
-    get = 'index'
-    create = 'create'
-    update = 'update'
+    get_all_files = 'get_all_files'
+    upload = 'upload'
 
 
 class Parameters(str, Enum):
-    id = 'id'
-    is_study = 'is_study'
-    study_status_id = 'study_status_id'
-    name = 'name'
-    branch_ids = 'branch_ids'
-    gender = 'gender'
-    age_from = 'age_from'
-    age_to = 'age_to'
-    phone = 'phone'
-    legal_type = 'legal_type'
-    legal_name = 'legal_name'
-    company_id = 'company_id'
-    lesson_count_from = 'lesson_count_from'
-    lesson_count_to = 'lesson_count_to'
-    balance_contract_from = 'balance_contract_from'
-    balance_contract_to = 'balance_contract_to'
-    balance_bonus_from = 'balance_bonus_from'
-    balance_bonus_to = 'balance_bonus_to'
-    removed = 'removed'
-    removed_from = 'removed_from'
-    removed_to = 'removed_to'
-    level_id = 'level_id'
-    assigned_id = 'assigned_id'
-    employee_id = 'employee_id'
-    lead_source_id = 'lead_source_id'
-    lead_status_id = 'lead_status_id'
-    color = 'color'
-    note = 'note'
-    date_from = 'date_from'
-    date_to = 'date_to'
-    next_lesson_date_from = 'next_lesson_date_from'
-    next_lesson_date_to = 'next_lesson_date_to'
-    tariff_till_from = 'tariff_till_from'
-    tariff_till_to = 'tariff_till_to'
-    customer_reject_id = 'customer_reject_id'
-    comment = 'comment'
-    dob = 'dob'
-    withGroups = 'withGroups'
-    page = 'page'
+    limit = 'limit'
+    media_type = 'media_type'
+    offset = 'offset'
+    fields = 'fields'
+    preview_size = 'preview_size'
+    preview_crop = 'preview_crop'
 
